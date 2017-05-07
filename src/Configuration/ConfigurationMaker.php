e53897eece0a69738a860f7df9a6f031bdc34d69
dbm01 <dav.brahim@gmail.com>
2017-05-08 00:31:04 +0200

    First commit

---
+++ src/Configuration/ConfigurationMaker.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace Configuration;
+
+/**
+ * Description of ConfigurationMaker
+ *
+ * @author dbm
+ */
+class ConfigurationMaker {
+    //put your code here
+    
+    public function export() {
+        echo "ok";
+    }
+}
